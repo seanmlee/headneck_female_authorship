b296c4a9ecdec575f7f01103044b41e7ef49ef22
seanmlee <82421211+seanmlee@users.noreply.github.com>
2024-01-11 11:38:49 -0500

    Create `prep.R`

---
+++ scripts/prep.R
@@ -0,0 +1,25 @@
+library("tidyverse")
+
+# read (Male = 0; Female = 1; Unfunded = 0; Funded = 1)
+author <- read.csv("data/author.csv", header = TRUE)
+
+# format
+author$Country.of.Origin <- 
+  factor(
+    author$Country.of.Origin, 
+    levels = c(
+      "US", 
+      "Non US"
+    )
+  )
+
+author$subtopic <- 
+  factor(
+    author$subtopic, 
+    levels = c(
+      "onc", 
+      "endo", 
+      "recon", 
+      "other"
+    )
+  )
